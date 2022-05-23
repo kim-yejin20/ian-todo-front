6d580835433fd1e62a78406a672fda4c5986e451
kim-yejin20 <jin11241124@gmail.com>
2022-05-23 11:29:50 +0900

    graphQL적용

--- components/todo/todo-context.tsx
+++ components/todo/todo-context.tsx
@@ -5,9 +5,9 @@ import React, {
   useState,
 } from 'react';
 
-import axios from 'axios';
 import { createContext } from 'react';
 import { TaskItemProps } from './TaskListItem';
+import { gql, useApolloClient } from '@apollo/client';
 
 export type TaskContextProps = {
   tasks: TaskItemProps[];
@@ -30,50 +30,157 @@ export const TaskContext = createContext<TaskContextProps>({
 
 export const TaskProvider: React.FC<PropsWithChildren<{}>> = (props) => {
   const [tasks, setTasks] = useState<TaskItemProps[]>([]);
+  const client = useApolloClient();
 
   useEffect(() => {
     async function getTasks() {
-      const newTasks = await axios.get('http://localhost:3000/tasks');
-      setTasks(newTasks.data);
+      const { data } = await client.query({
+        query: gql`
+          query GET_TASKS {
+            tasks {
+              id
+              content
+              isCompleted
+            }
+          }
+        `,
+      });
+      setTasks(data.tasks);
     }
     getTasks();
   }, []);
 
+  // const CreateTask = async (content: string) => {
+  //   const res = await axios.post('http://localhost:3000/tasks', {
+  //     content,
+  //   });
+  //   setTasks([res.data, ...tasks]);
+  // };
+
+  // mutation CREATE_TASK($input: TaskInput!) {
+  //   createTask(input:$input) {
+  //     id
+  //     content
+  //     isCompleted
+  //   }
+  // }
+
   const CreateTask = async (content: string) => {
-    const res = await axios.post('http://localhost:3000/tasks', {
-      content,
+    const { data } = await client.mutate({
+      mutation: gql`
+        mutation CREATE_TASK($input: TaskInput!) {
+          createTask(input: $input) {
+            id
+            content
+            isCompleted
+          }
+        }
+      `,
+      variables: {
+        input: {
+          content,
+        },
+      },
     });
-    setTasks([res.data, ...tasks]);
+    console.log(data);
+    setTasks((prev) => [data.createTask, ...prev]);
   };
 
-  async function DeleteTask(id: number) {
-    await axios.delete(`http://localhost:3000/tasks/${id}`);
+  // async function DeleteTask(id: number) {
+  //   await axios.delete(`http://localhost:3000/tasks/${id}`);
+  //   const newTasks = tasks.filter((task) => task.id !== id);
+  //   setTasks(newTasks);
+  // }
+
+  const DeleteTask = async (id: number) => {
+    const { data } = await client.mutate({
+      mutation: gql`
+        mutation DELETE_TASK($id: ID!) {
+          deleteTask(id: $id)
+        }
+      `,
+      variables: {
+        id,
+      },
+    });
+    console.log(data);
     const newTasks = tasks.filter((task) => task.id !== id);
     setTasks(newTasks);
-  }
+  };
 
-  async function UpdateTask(id: number, content: string) {
-    const res = await axios.put(`http://localhost:3000/tasks/${id}`, {
-      content: content,
+  // async function UpdateTask(id: number, content: string) {
+  //   const res = await axios.put(`http://localhost:3000/tasks/${id}`, {
+  //     content: content,
+  //   });
+
+  //   const newTasks = tasks.map((task) =>
+  //     task.id === res.data.id ? { ...task, content: task.content } : task
+  //   );
+  //   setTasks(newTasks);
+  // }
+
+  const UpdateTask = async (id: number, content: string) => {
+    const { data } = await client.mutate({
+      mutation: gql`
+        mutation UPDATE_TASK($id: ID!, $input: TaskInput!) {
+          updateTask(id: $id, input: $input) {
+            id
+            content
+            isCompleted
+          }
+        }
+      `,
+      variables: {
+        id: id,
+        input: {
+          content,
+        },
+      },
     });
-
+    console.log(data);
     const newTasks = tasks.map((task) =>
-      task.id === res.data.id ? { ...task, content: task.content } : task
+      task.id === data.updateTask.id ? { ...task, content: task.content } : task
     );
     setTasks(newTasks);
-  }
+  };
 
-  async function DoneTask(id: number, check: boolean) {
-    const res = await axios.put(`http://localhost:3000/tasks/${id}`, {
-      isCompleted: !check,
+  // async function DoneTask(id: number, check: boolean) {
+  //   const res = await axios.put(`http://localhost:3000/tasks/${id}`, {
+  //     isCompleted: !check,
+  //   });
+  //   const newTasks = tasks.map((task) =>
+  //     task.id === res.data.id
+  //       ? { ...task, isCompleted: task.isCompleted }
+  //       : task
+  //   );
+  //   setTasks(newTasks);
+  // }
+
+  const DoneTask = async (id: number, check: boolean) => {
+    const { data } = await client.mutate({
+      mutation: gql`
+        mutation DONE_TASK($id: ID!, $input: TaskInput!) {
+          updateTask(id: $id, input: $input) {
+            id
+            content
+            isCompleted
+          }
+        }
+      `,
+      variables: {
+        id: id,
+        input: {
+          isCompleted: !check,
+        },
+      },
     });
+
+    console.log(data);
     const newTasks = tasks.map((task) =>
-      task.id === res.data.id
-        ? { ...task, isCompleted: task.isCompleted }
-        : task
+      task.id === data.updateTask.id ? { ...task, content: task.content } : task
     );
     setTasks(newTasks);
-  }
+  };
 
   return (
     <TaskContext.Provider
